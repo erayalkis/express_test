e56a2f3f9a324095ec1c4de40079b9efdb9b08e1
Lily <lolpo12345@outlook.com>
2022-04-30 19:13:13 +0300

    Create get route

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
 const express = require("express");
 const app = express();
 
-app.listen(3000);
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.listen(PORT);
+console.log(`Listening on http://localhost:${PORT}`);
