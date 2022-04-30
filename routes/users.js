13e1e87f5d2904e0c43f9ccdc741aa471aa88621
Lily <lolpo12345@outlook.com>
2022-04-30 20:45:25 +0300

    Set up routes for creation, deletion and editing of users

--- routes/users.js
+++ routes/users.js
@@ -13,4 +13,16 @@ router.get("/new", (req, res) => {
   res.status(200).send("New user form");
 });
 
+router
+  .route("/:id")
+  .get((req, res) => {
+    res.send(`Get user with id ${req.params.id}`);
+  })
+  .put((req, res) => {
+    res.send(`Edit user with id ${req.params.id}`);
+  })
+  .delete((req, res) => {
+    res.send(`Delete user with id ${req.params.id}`);
+  });
+
 module.exports = router;
