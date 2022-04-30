ada20b0c464ae25a9c31966d349ab9f2975c9d2c
Lily <lolpo12345@outlook.com>
2022-04-30 20:28:01 +0300

    Create post request for user creation

--- routes/users.js
+++ routes/users.js
@@ -5,6 +5,10 @@ router.get("/", (req, res) => {
   res.status(200).send("Users list");
 });
 
+router.post("/", (req, res) => {
+  res.status(200).send("Create user");
+});
+
 router.get("/new", (req, res) => {
   res.status(200).send("New user form");
 });
