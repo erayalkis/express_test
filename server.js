eb70c6d168851def444a3ee421653e809917a276
Lily <lolpo12345@outlook.com>
2022-04-30 19:34:15 +0300

    Create defaults for embeds

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = 3000;
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.status(200).render("index", { text: ", World!" });
+  res.status(200).render("index", { text: "Me!" });
 });
 
 app.listen(PORT);
