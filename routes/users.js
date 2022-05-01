aba2453cda12e79eb3f938b44657aac5ac3c11da
Lily <lolpo12345@outlook.com>
2022-05-01 03:09:40 +0300

    Set up user logger

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
+router.use(logger); // Will run the logger for each path
+// If you wanted to run it for just a specific path, you need to add it as a function
+// Ex. router.get("/", logger, (req, res) => {...}) <- This would run the logger for this route only
+
 router.get("/", (req, res) => {
   res.status(200).send("Users list");
 });
@@ -24,5 +28,10 @@ router
   .delete((req, res) => {
     res.send(`Delete user with id ${req.params.id}`);
   });
+
+function logger(req, res, next) {
+  console.log(req.originalUrl);
+  next();
+}
 
 module.exports = router;
